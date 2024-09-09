ff3a32edc85f42d9686963241418c759cc342bdb
andrefelizardo <andreluis.felizardo@outlook.com>
2024-09-09 19:46:26 -0300

    Refactor user_controller.go and add CreateUserRequest struct

--- internal/controllers/user_controller.go
+++ internal/controllers/user_controller.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/andrefelizardo/todo-api/internal/request"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type UserController struct{
@@ -15,6 +18,21 @@ func NewUserController() *UserController {
 }
 
 func (u *UserController) CreateUser(ctx *gin.Context) {
+
+	var input request.CreateUserRequest
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	validate := validator.New()
+	err := validate.Struct(input)
+	if err != nil {
+		errors := err.(validator.ValidationErrors)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Validation error: %s", errors)})
+		return
+	}
 	
 	ctx.JSON(http.StatusCreated, gin.H{
 		"message": "User created",
