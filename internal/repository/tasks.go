275c932f66c03e476d1145b68fe3c6be0b9878d4
andrefelizardo <andreluis.felizardo@outlook.com>
2024-11-24 09:46:39 -0300

    Add timestamps to task creation: include created_at and updated_at fields

--- internal/repository/tasks.go
+++ internal/repository/tasks.go
@@ -24,13 +24,14 @@ func NewTasksRepository(db *sql.DB) *tasksRepositoryImpl {
 func (t *tasksRepositoryImpl) Create(task dto.CreateTaskInput) (model.Task, error) {
 	id := uuid.New().String()
 	var dueDate time.Time
+	var now = time.Now()
 	if task.DueDate == nil {
-		dueDate = time.Now()
+		dueDate = now
 	} else {
 		dueDate = *task.DueDate
 	}
-	_, err := t.db.Exec("INSERT into tasks (id, title, description, status, due_date) VALUES ($1, $2, $3, $4, $5)",
-						id, task.Title, task.Description, model.StatusPending, dueDate)
+	_, err := t.db.Exec("INSERT into tasks (id, title, description, status, due_date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+						id, task.Title, task.Description, model.StatusPending, dueDate, now, now)
 	if err != nil {
 		return model.Task{}, err
 	}
@@ -40,5 +41,7 @@ func (t *tasksRepositoryImpl) Create(task dto.CreateTaskInput) (model.Task, erro
 		Description: task.Description,
 		DueDate: dueDate,
 		Status: model.StatusPending,
+		CreatedAt: now,
+		UpdatedAt: now,
 	}, nil
 }
