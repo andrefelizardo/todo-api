79558fce47badab168a51dc2883515ad4e9084ca
andrefelizardo <andreluis.felizardo@outlook.com>
2024-11-24 09:49:33 -0300

    Remove unused status field from CreateTaskInput struct

--- internal/dto/tasks.go
+++ internal/dto/tasks.go
@@ -5,6 +5,5 @@ import "time"
 type CreateTaskInput struct {
 	Title string `json:"title"`
 	Description *string `json:"description"`
-	// Status string `json:"status"`
 	DueDate *time.Time `json:"due_date"`
 }
